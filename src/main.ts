02d317ce8b12de2a1914f95683999893ee1fda12
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-06 13:25:39 -0300

    Clase N°9: Number, Boolean y String.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
-console.log('Hola Platzi.');
+// Number
+// Explicito
+
+let phone: number;
+phone = 1;
+phone = 54234567;
+// phone = 'Hola'; // Error
+
+// Inferido
+let phoneNumber = 54234567;
+phoneNumber = 1231;
+// phoneNumber = true; // Error por tipo de dato
+
+let hex: number = 0x0a124bcdf;
+let binariy: number = 0b00110101;
+let octal: number = 0o0771234;
+
+// Boolean
+// Tipado Explicito
+let isPro: boolean;
+isPro = true;
+
+// Tipado inferido
+let isUserPro = false;
+isUserPro = Boolean(false);
+
+// String
+// Tipo explicito
+let username: string = 'Galeed';
+// userName = true; // Error por tipo de dato
+
+// Template String
+// Uso de back-tick `
+let userInfo: string;
+userInfo = `
+    User Info:
+    username: ${username}
+    fistName: ${username} Gutierrez
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+
+console.log(userInfo)
