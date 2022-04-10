d7f630f58c44b1a5890144473a0c6db73da942cd
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-10 00:25:46 -0300

    Clase N°22: Interfaces: propiedades opcionales.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -23,10 +23,46 @@ const myPic = {
     orientation: PhotoOrientation.Landscape,
 };
 
-showPicture(myPic);
-showPicture({
-    title: 'Test Title 2',
-    date: '2022-04',
-    orientation: PhotoOrientation.Portrait,
-    // extra: 'test',
-});
+// showPicture(myPic);
+// showPicture({
+//     title: 'Test Title 2',
+//     date: '2022-04',
+//     orientation: PhotoOrientation.Portrait,
+//     // extra: 'test',
+// });
+
+interface PictureConfig {
+    title?: string;
+    date?: string;
+    orientation?: PhotoOrientation;
+}
+
+function generatePicture (config: PictureConfig) {
+    const pic = {title: 'Defaul', date: '2020'};
+    if (config.title) {
+        pic.title = config.title;
+    }
+    if (config.date) {
+        pic.date = config.date;
+    }
+    return pic
+}
+
+let picture = generatePicture({});
+picture = generatePicture({title: 'Travel Pic'});
+picture = generatePicture({title: 'Travel Pic', date: '2020-04'});
+// console.log({picture});
+
+// Interfaz: Usuario
+interface User {
+    readonly id: number;
+    username: string;
+    isPro: boolean;
+}
+
+let user: User;
+user = {id: 10, username: 'galeed', isPro: true};
+console.log({user});
+user.username = 'XD';
+// user.id = 20; // Error
+console.log({user});
